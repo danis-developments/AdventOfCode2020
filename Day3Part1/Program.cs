b2d3afee9d320709f4687d1216df5654120d486c
Dan Martz <dansdevelopments@gmail.com>
2020-12-03 18:38:13 -0600

    Day 3 Part 1 complete

---
+++ Day3Part1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day3Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _fname = "mapslice.txt";
+            string[] lines = System.IO.File.ReadAllLines(@_fname);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var accumulator = "";
+                for (int j = 0; j < 32; j++)
+                {
+                    accumulator += line;
+                }
+                lines[i] = accumulator;         
+            }
+            Console.WriteLine($"we have {lines.Length} lines of {lines[0].Length} length");
+            var rise = 1;
+            var run = 3;
+            var x = 0;
+            var y = 0;
+            var numTrees = 0;
+            var loopLength = lines.Length / rise;
+            char tree = '#';
+            if(lines[y][x] == tree){
+                numTrees++;
+            }
+            for (int i = 0; i < loopLength; i++)
+            {
+                if(lines[y][x] == tree){
+                    numTrees++;
+                }
+                y += rise;
+                x += run;
+            } 
+            Console.WriteLine($"Number of trees we hit on the way down is {numTrees}");
+        }
+    }
+}
